5773895c24a578c711a015e6161c4e3f8f918df0
github username <github email address>
2018-07-11 13:23:10 -0400

    cartooned

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|calls| calls.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #My original code below - was trying to combine and subtract arrays
+  #new_food = (food << cheese_types).flatten!
+  #new_food.uniq - food
+
+  #second try below - tried to combine arrays and find duplicates
+  food.concat cheese_types
+  food.detect {|items| food.count(items) > 1}
+
+
 end
